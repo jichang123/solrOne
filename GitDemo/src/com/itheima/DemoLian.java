caf290b83caa4bf0afc69bd39310871f6f3e636e
jichang123 <946349569@qq.com>
2018-05-14 17:55:15 +0800

    李四开始修改

--- GitDemo/src/com/itheima/DemoLian.java
+++ GitDemo/src/com/itheima/DemoLian.java
@@ -5,5 +5,6 @@ public class DemoLian {
 	public static void main(String[] args) {
 		System.out.println("第一个版本---来来来");
 		System.out.println("第二个版本---去去去");
+		System.out.println("李四开始修改啦");
 	}
 }
